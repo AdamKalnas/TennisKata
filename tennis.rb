5d8444a8a7d0d203be0b33bdd322490d179291f6
Adam Kalnas <a.kalnas@modcloth.com>
2013-07-01 09:06:32 -0400

    refactoring to prepare for the next use case

--- tennis.rb
+++ tennis.rb
@@ -7,12 +7,21 @@ class Tennis
 
   def point_scored(player_number)
     if player_number == 1
-      @player1_score = 15
+      @player1_score = increase_score(@player1_score)
     elsif player_number == 2
-      @player2_score = 15
+      @player2_score = increase_score(@player2_score)
     end
   end
   
+  def increase_score(current_score)
+    case current_score
+    when 0
+      15
+    end
+      
+    
+  end
+
   def score 
     "#{@player1_score}-#{@player2_score}"
   end
